0d4bbb7187328dcc26ac246c27b53c0d9d4eaede
DhavalContrivertech <dhaval@contrivertech.com>
2022-06-18 17:28:36 +0530

    LOcation Not done

--- Controller/LocationController.js
+++ Controller/LocationController.js
@@ -3,6 +3,9 @@ const User = require("../model/User")
 
 exports.FindNearProductLocation = async (req, res) => {
     try {
+        var AllProduct = [];
+        
+        let array = []
         const pincode = req.params.pincode
         const Location = await User.find({ Pincode :  pincode , Role : "Vendor"});
 
@@ -10,35 +13,33 @@ exports.FindNearProductLocation = async (req, res) => {
             res.status(400).json({error : "Location data is not found"})
             return
         }
-        console.log(Location)
-        // const finduser = await User.find()
-        let AllProduct = [];
-        let data;
+
+        console.log(Location , "location")
         if (Location) {
-            Location.forEach(async  (ele , ind) => {
+
+            Location.forEach(async (ele , ind) => {
+
                 const productwithlocation = await Product.find({VendorId : Location[ind].id})
-                console.log(productwithlocation)
-                productwithlocation && productwithlocation.map(ele => {
-                    // console.log(ele)
-                    data = ele
-                    AllProduct.push({...data})
-                    console.log(AllProduct , 'All Products')
-                    res.status(400).json({message : "Location data is found" , AllProduct})
-                    return
-                    // AllProduct.push(ele)
+                    
+                productwithlocation.filter((ele , ind) => {
+                    AllProduct.push(ele)
+                    array.push(AllProduct)
+                    console.log(array , "arra ins")
                 })
-                // console.log(data)
-                // productwithlocation.map((ele) => {
-                    //     let data = ele;
-                    //     console.log(data)
-                    // })
-                })
-            // console.log(data)
+
+
+            })
+
+            console.log(array , "arra out")
+            // console.log(newdata , "All Product ")
+            res.status(200).json({message : "Location data is found" , array})
+                // console.log(AllProduct , "All Product")
             return
         }
 
 
     } catch (error) {
+        console.log(error)
         res.status(400).json({error : "We unable to Find data"})
     }
 }

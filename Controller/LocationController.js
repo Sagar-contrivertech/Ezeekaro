70513a52145f88ef2078672b1391fb36bfa272f5
Sagar-contrivertech <97974995+Sagar-contrivertech@users.noreply.github.com>
2022-06-21 16:10:26 +0530

    chnage in location

--- Controller/LocationController.js
+++ Controller/LocationController.js
@@ -4,42 +4,32 @@ const User = require("../model/User")
 exports.FindNearProductLocation = async (req, res) => {
     try {
         var AllProduct = [];
-        
+
         let array = []
         const pincode = req.params.pincode
-        const Location = await User.find({ Pincode :  pincode , Role : "Vendor"});
+        const Location = await User.find({ Pincode: pincode, Role: "Vendor" });
 
         if (!Location) {
-            res.status(400).json({error : "Location data is not found"})
+            res.status(400).json({ error: "Location data is not found" })
             return
         }
-
-        console.log(Location , "location")
+        // console.log(Location , "location")
         if (Location) {
-
-            Location.forEach(async (ele , ind) => {
-
-                const productwithlocation = await Product.find({VendorId : Location[ind].id})
-                    
-                productwithlocation.filter((ele , ind) => {
+            for (let index = 0; index < Location.length; index++) {
+                const productwithlocation = await Product.find({ VendorId: Location[index].id })
+                productwithlocation.map((ele, ind) => {
                     AllProduct.push(ele)
-                    array.push(AllProduct)
-                    console.log(array , "arra ins")
+                    return AllProduct
                 })
-
-
-            })
-
-            console.log(array , "arra out")
-            // console.log(newdata , "All Product ")
-            res.status(200).json({message : "Location data is found" , array})
-                // console.log(AllProduct , "All Product")
+            }
+            console.log(AllProduct);
+            res.status(200).json({ message: "Location data is found", AllProduct })
             return
         }
 
 
     } catch (error) {
         console.log(error)
-        res.status(400).json({error : "We unable to Find data"})
+        res.status(400).json({ error: "We unable to Find data" })
     }
 }
